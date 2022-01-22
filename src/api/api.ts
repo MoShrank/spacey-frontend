89a8bf3011e254e96f4a900368a5325c809fc40a
Moritz Eich <hey@moritz.dev>
2022-01-23 00:03:00 +0100

    add: timeout to http requests

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,7 @@
+import GlobalError from "events/globalError";
+
+type Method = "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "HEAD" | "OPTIONS";
+
 class API {
     private static _headers: Headers = new Headers({
         "Content-Type": "application/json",
@@ -7,46 +11,54 @@ class API {
         ? process.env.REACT_APP_BASE_URL
         : "http://localhost:8080/";
 
-    static async GET(url: string): Promise<{ [key: string]: any }> {
-        const res = await fetch(`${this._baseUrl}/${url}`, {
-            method: "GET",
-            headers: this._headers,
-        });
+    private static async Request(
+        url: string,
+        method: Method,
+        body?: Object
+    ): Promise<{ [key: string]: any }> {
+        const controller = new AbortController();
 
-        if (!res.ok) {
-            throw new Error(res.statusText);
+        setTimeout(() => controller.abort(), 5000);
+
+        try {
+            const res = await fetch(`${this._baseUrl}/${url}`, {
+                method: method,
+                headers: this._headers,
+                body: JSON.stringify(body),
+                signal: controller.signal,
+            });
+
+            if (!res.ok) {
+                throw new Error(res.statusText);
+            }
+
+            const resBody = await res.json();
+            return resBody.data;
+        } catch (err) {
+            GlobalError.emit(
+                "timeout. Either the server or your internet connection is down."
+            );
+            throw err;
         }
+    }
 
-        const resBody = await res.json();
-        return resBody.data;
+    static async GET(url: string): Promise<{ [key: string]: any }> {
+        return API.Request(url, "GET");
     }
 
     static async POST(
         url: string,
         body: Object
     ): Promise<{ [key: string]: any }> {
-        const res = await fetch(`${this._baseUrl}/${url}`, {
-            method: "POST",
-            headers: this._headers,
-            body: JSON.stringify(body),
-        });
-
-        if (!res.ok) {
-            throw new Error(res.statusText);
-        }
-
-        const resBody = await res.json();
-        return resBody.data;
+        return API.Request(url, "POST", body);
     }
 
-    /*
-    static async Put(url, headers, body) {
-        return new Request("PUT", url, headers, body);
+    static async PUT(url: string, body: Object) {
+        return API.Request(url, "PUT", body);
     }
-    static async Delete(url, headers, body) {
-        return new Request("DELETE", url, headers, body);
+    static async Delete(url: string) {
+        return API.Request(url, "DELETE");
     }
-    */
 }
 
 export default API;
