570b5220ddddb72bfd0a6194b31379280b503d69
Moritz Eich <hey@moritz.dev>
2022-02-05 11:02:06 +0100

    add: char limit on text area

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -3,12 +3,17 @@ import "./style.scss";
 interface TextProps {
 	children: React.ReactNode;
 	color: "darkblue" | "lightgrey";
+	className?: string;
 }
 
 const Text = (props: TextProps) => {
 	const color = props.color === "darkblue" ? "#417db5" : "#8c8c8c";
 
-	return <p style={{ color: color }}>{props.children}</p>;
+	return (
+		<p className={props.className} style={{ color: color }}>
+			{props.children}
+		</p>
+	);
 };
 
 export default Text;
