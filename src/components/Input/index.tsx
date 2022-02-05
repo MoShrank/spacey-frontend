18357dccc36c20b424a623b283e7177ae1373097
Moritz Eich <hey@moritz.dev>
2022-02-05 18:01:50 +0100

    fix: textarea label

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,12 @@
 import "./style.scss";
 
+/*
+children slicing is done because first children is supposed to be
+the input element itself while the rest can be something else
+(e.g. char limit on textarea) this is probably not the most elegant way
+and should be refactored in the future
+*/
+
 const Input = ({
 	error,
 	placeholder,
@@ -11,11 +18,12 @@ const Input = ({
 }) => {
 	return (
 		<div className={`text_input_container ${error && "error"}`}>
-			{children}
+			{Array.isArray(children) ? children[0] : children}
 			<div className="label_overlay_container">
 				<span className="line" />
 				<label htmlFor={placeholder}>{placeholder}</label>
 			</div>
+			{Array.isArray(children) ? children.slice(1) : null}
 		</div>
 	);
 };
