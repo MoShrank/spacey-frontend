53d59967916ee3d28c5a38a2336d78d8074a9de7
Moritz Eich <hey@moritz.dev>
2022-03-29 21:47:47 +0200

    add: new card design + spacer component.

--- src/components/EditableCard/index.tsx
+++ src/components/EditableCard/index.tsx
@@ -1,7 +1,6 @@
 import CardContainer from "components/CardContainer";
 import Form from "components/Form";
 import Text from "components/Text";
-import { useEffect } from "react";
 import { DeckI } from "types/deck";
 
 import style from "./style.module.scss";
@@ -18,15 +17,6 @@ interface EditableCardI {
 	onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 }
 
-const resize = (target: HTMLTextAreaElement) => {
-	target.style.height = "1.8rem";
-	target.style.height = target.scrollHeight + "px";
-};
-
-const handleResize = (e: React.FormEvent<HTMLTextAreaElement>) => {
-	resize(e.target as HTMLTextAreaElement);
-};
-
 const EditableCard = ({
 	children,
 	card,
@@ -35,28 +25,19 @@ const EditableCard = ({
 	onAnswerInput,
 	onSubmit,
 }: EditableCardI) => {
-	useEffect(() => {
-		resize(document.getElementById("question") as HTMLTextAreaElement);
-		resize(document.getElementById("answer") as HTMLTextAreaElement);
-	});
-
 	return (
 		<Form onSubmit={onSubmit}>
 			<Text color="darkblue">{deck.name}</Text>
 			<CardContainer color={deck.color}>
 				<textarea
-					id="question"
 					className={`${style.textarea} ${style.question}`}
 					value={card.question}
-					onInput={handleResize}
 					onChange={onQuestionInput}
 					placeholder="question"
 				/>
 				<textarea
-					id="answer"
 					className={`${style.textarea} ${style.answer}`}
 					value={card.answer}
-					onInput={handleResize}
 					onChange={onAnswerInput}
 					placeholder="answer"
 				/>
