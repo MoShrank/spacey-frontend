5f2282c941af98ef95a1a1dbbd6a0d8eec14b896
kerem-kaynak <93583929+kerem-kaynak@users.noreply.github.com>
2022-04-18 11:36:15 +0200

    Mobile header update/spa 92 (#26)
    change: mobile header

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,27 +1,28 @@
-import { ReactComponent as MenuIcon } from "assets/icons/menu.svg";
+import { ReactComponent as MenuIcon } from "assets/icons/accountIcon.svg";
 import { ReactComponent as HomeIcon } from "assets/img/logo_simple.svg";
-import BurgerMenu from "components/BurgerMenu";
+import AccountMenu from "components/AccountMenu";
 import useOnClickOutside from "hooks/useClickOutside";
 import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { useGlobalState } from "store/store";
 
 import style from "./style.module.scss";
 
 const Navbar = () => {
 	const [menuOpen, setMenuOpen] = useState(false);
 	const menuRef = useRef(null);
+	const [isLoggedIn] = useGlobalState("isLoggedIn");
 	useOnClickOutside(menuRef, () => setMenuOpen(false));
 
 	return (
 		<nav className={style.container}>
-			<Link className={style.home_icon} to="/">
-				<HomeIcon />
-			</Link>
-			{menuOpen ? (
-				<BurgerMenu onClose={() => setMenuOpen(false)} />
-			) : (
-				<MenuIcon onClick={() => setMenuOpen(true)} />
+			{isLoggedIn && (
+				<Link className={style.home_icon} to="/">
+					<HomeIcon />
+				</Link>
 			)}
+			{isLoggedIn && <MenuIcon onClick={() => setMenuOpen(true)} />}
+			{menuOpen && <AccountMenu onClose={() => setMenuOpen(false)} />}
 		</nav>
 	);
 };
