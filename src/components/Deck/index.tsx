048427f54a7496dff53216a61f8df7fa2ba9da90
Moritz Eich <hey@moritz.dev>
2022-06-03 15:40:00 +0200

    add: responsive deck overview design

--- src/components/Deck/index.tsx
+++ src/components/Deck/index.tsx
@@ -4,20 +4,21 @@ import MemoryStabilityIndicator from "components/MemoryStabilityIndicator";
 import { Link } from "react-router-dom";
 import { DeckI } from "types/deck";
 
-import "./style.scss";
+import style from "./style.module.scss";
 
 const Deck = ({ deck }: { deck: DeckI }) => {
 	return (
-		<Link key={deck.id} to={`decks/${deck.id}`} className="deck_container">
-			<div style={{ background: deck.color }} className="deck_header"></div>
-			<div className="deck_body">
+		<Link key={deck.id} to={`decks/${deck.id}`} className={style.deck_container}>
+			<div style={{ background: deck.color }} className={style.deck_header}></div>
+			<div className={style.deck_body}>
 				<Header kind="h3">{deck.name}</Header>
-				<div className="deck_info">
-					<CardCount count={deck.cards.length} />
+				<CardCount count={deck.cards.length} />
+				<div className={style.indicator}>
 					<MemoryStabilityIndicator
 						probability={deck.averageRecallProbability}
 						styles={{ width: "24px", height: "24px" }}
-						fill={"darkblue"}></MemoryStabilityIndicator>
+						fill={"darkblue"}
+					/>
 				</div>
 			</div>
 		</Link>
