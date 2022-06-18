f3637f64743e5a27e2672ea92ef4cd8b8931c060
Moritz Eich <hey@moritz.dev>
2022-06-18 23:43:53 +0200

    fix: verify email design

--- src/pages/VerifyEmail/index.tsx
+++ src/pages/VerifyEmail/index.tsx
@@ -4,6 +4,7 @@ import Header from "components/Header";
 import Layout from "components/Layout";
 import Spacer from "components/Spacer";
 import Text from "components/Text";
+import { useState } from "react";
 import { Navigate } from "react-router-dom";
 import { useGlobalState } from "store/store";
 import { UserI } from "types/user";
@@ -12,26 +13,34 @@ import style from "./style.module.scss";
 
 const VerifyEmail = () => {
 	const [user] = useGlobalState<UserI>("user");
+	const [disableButton, setDisableButton] = useState(false);
 
 	if (user.emailValidated) return <Navigate to="/" />;
 
 	const sendEmail = () => {
 		sendVerificationEmail();
+		setDisableButton(true);
 	};
 
 	return (
 		<Layout>
 			<div className={style.content}>
 				<Header kind="h2">Thanks for signing up for Spacey</Header>
-				<Spacer spacing={8} />
+				<Spacer spacing={4} />
 				<Text>
 					We just sent you an e-mail. To start your learning journey please use the
-					link in the e-mail to validate your account. Make sure to also check the
+					link in the e-mail to validate your account. Make sure to also check your
 					spam folder.
+				</Text>
+				<Spacer spacing={2} />
+				<Text>
+					Sent to <span className={style.email}>{user.email}</span>
 				</Text>
 			</div>
 
-			<Button onClick={sendEmail}>Resend Email</Button>
+			<Button disabled={disableButton} onClick={sendEmail}>
+				Resend Email
+			</Button>
 		</Layout>
 	);
 };
