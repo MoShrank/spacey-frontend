04a635e965fe21f0746e279e651145dbd92fc8d9
Moritz Eich <m.eich@dental21.de>
2024-04-03 23:06:08 +0200

    add: focus/reader mode

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,10 +2,17 @@ import ReactDOM from "react-dom";
 
 interface ModalI {
 	children: React.ReactNode;
+	nodeID?: string;
 }
 
-const Modal = ({ children }: ModalI) => {
-	const node = document.getElementById("modal-root") as HTMLElement;
+const Modal = ({ children, nodeID = "modal-root" }: ModalI) => {
+	let node = document.getElementById(nodeID);
+
+	if (!node) {
+		node = document.createElement("div");
+		node.setAttribute("id", nodeID);
+		document.body.appendChild(node);
+	}
 
 	return ReactDOM.createPortal(children, node);
 };
